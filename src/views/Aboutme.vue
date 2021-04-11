df0847e943ed530a15f57ba2773999065239a7b4
David Bruere <david.bruere@data-spree.com>
2021-04-11 11:18:24 +0200

    work progress

--- src/views/Aboutme.vue
+++ src/views/Aboutme.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="container">
     <h2 class="headline">Profil Website</h2>
-    <div class="row m-5">
-      <div class="col-4">
+    <div class="row m-5 justify-content-between">
+      <div class="col-lg-4">
         <b-card class="card-first-row" fluid title="About me" >
           <div class="card-text">Es freut mich dein Interesse geweckt zu haben. Mein name ist David Bruere und ich bin Frondend Developer.
             Ich wohne in Berlin bin tooootal toll (wie ein tollwuetiger hund).Weiterer Text und blabla</div>
         </b-card>
       </div>
-      <div class="col-4">
+      <div class="col-lg-4">
         <b-img :src="require('../assets/ich.jpg')" thumbnail fluid rounded="circle"
                alt="picture could not load" class="my-image"></b-img>
       </div>
-      <div class="col-4">
+      <div class="col-lg-4">
         <b-card class="card-first-row" border-variant="light" title="short overview">
             <div class="row">
               <b-col>
@@ -20,6 +20,7 @@
               <li>Informatiker</li>
               <li>Web Apps</li>
               <li>Frontend</li>
+              <li>Junior</li>
             </ul>
               </b-col>
               <b-col>
@@ -46,6 +47,7 @@ export default {
 .headline{
   color:white;
   margin-top: 10px;
+  text-align: center;
 }
 .ul-overview{
   text-align: left;
